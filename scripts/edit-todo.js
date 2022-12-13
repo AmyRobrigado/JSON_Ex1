487f7810f7b40cbf2cd870e1333aa124b9536d84
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-13 16:00:07 -0500

    Created button functions and hide/show functions

--- scripts/edit-todo.js
+++ scripts/edit-todo.js
@@ -1,5 +1,44 @@
 "use strict"
 
 window.onload = function(){
+    showInfoField(); 
+    hideInfoField(); 
+
+    document.getElementById("updateBtn").onclick = updateButtonOnClick; 
+    document.getElementById("cancelBtn").onclick = cancelButtonOnClick; 
+}
+
+
+
+function updateButtonOnClick(){
+    let inputValue = document.getElementById("updateBtn").value; 
+
+    if (inputValue == todo[i].id)
+
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(response => response.json())
+        .then(todo => {
+            
+        })
+}
+
+function cancelButtonOnClick(){
     
 }
+
+function showInfoField(){
+
+}
+
+function hideInfoField(){
+
+}
+
+
+
+
+
+// function that disables the form when "retrieve" is pressed 
+// function that hides and shows the info fields 
+// create and wire the "cancel" button 
+// create the update button 
