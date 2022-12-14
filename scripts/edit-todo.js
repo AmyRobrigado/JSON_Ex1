26aa9a67676c3cd54483b8fcd13d0b1c0a3b18ab
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-14 09:11:54 -0500

    2 functions working

--- scripts/edit-todo.js
+++ scripts/edit-todo.js
@@ -3,12 +3,18 @@
 window.onload = function(){
     showInfoField(); 
     hideInfoField(); 
-
+    
+    document.getElementById("retrieveBtn").onclick = retrieveButtonOnClick; 
     document.getElementById("updateBtn").onclick = updateButtonOnClick; 
     document.getElementById("cancelBtn").onclick = cancelButtonOnClick; 
 }
 
-
+function retrieveButtonOnClick(){
+    let
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(response => response.json())
+        .then
+}
 
 function updateButtonOnClick(){
     let inputValue = document.getElementById("updateBtn").value; 
@@ -23,16 +29,16 @@ function updateButtonOnClick(){
 }
 
 function cancelButtonOnClick(){
-    
+    window.location.href= "../pages/index.html"
 }
 
-function showInfoField(){
+/* function showInfoField(){
+    document.getElementById("infoContainer").style.display = "block"; 
+} */
 
-}
-
-function hideInfoField(){
-
-}
+/* function hideInfoField(){
+    document.getElementById("infoContainer").style.display = "none"; 
+} */
 
 
 
